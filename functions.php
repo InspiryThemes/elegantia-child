bc1dc9025b4be045e11eeef5ee80fd89625e58aa
Tariq Khan <tariq@inspirythemes.com>
2019-04-16 15:16:49 +0500

    Added child theme

--- functions.php
+++ functions.php
@@ -1,14 +1,46 @@
 <?php
 /*-----------------------------------------------------------------------------------*/
-/*	Load Child Theme Styles
+/*	Enqueue Styles in Child Theme
 /*-----------------------------------------------------------------------------------*/
-if(!function_exists('load_child_theme_styles')){
-    function load_child_theme_styles(){
-        if (!is_admin()){
-            ?>
-            <link rel='stylesheet' id='child-custom-css'  href='<?php echo get_stylesheet_directory_uri() . '/child-custom.css'; ?>' type='text/css' media='all' />
-            <?php
-        }
-    }
+if ( ! function_exists( 'inspiry_enqueue_child_styles' ) ) {
+
+	function inspiry_enqueue_child_styles() {
+
+		if ( ! is_admin() ) {
+
+			// dequeue and deregister parent default css
+			wp_dequeue_style( 'parent-default' );
+			wp_deregister_style( 'parent-default' );
+
+			// dequeue parent custom css
+			wp_dequeue_style( 'parent-custom' );
+
+			// parent default css
+			wp_enqueue_style( 'parent-default', get_template_directory_uri() . '/style.css' );
+
+			// parent custom css
+			wp_enqueue_style( 'parent-custom' );
+
+			// child default css */
+			wp_enqueue_style( 'child-default', get_stylesheet_uri(), array( 'parent-default' ), '1.0', 'all' );
+
+			// child custom css
+			wp_enqueue_style( 'child-custom', get_stylesheet_directory_uri() . '/css/child-custom.css', array( 'parent-default' ), '1.0', 'all' );
+
+			// child custom js
+			wp_enqueue_script( 'child-custom', get_stylesheet_directory_uri() . '/js/child-custom.js', array( 'jquery' ), '1.0', true );
+		}
+	}
 }
-add_action( 'wp_head', 'load_child_theme_styles', PHP_INT_MAX );
+add_action( 'wp_enqueue_scripts', 'inspiry_enqueue_child_styles', PHP_INT_MAX );
+
+
+if ( ! function_exists( 'inspiry_elegantia_child_theme_setup' ) ) {
+	/**
+	 * Load translation files from child theme
+	 */
+	function inspiry_elegantia_child_theme_setup() {
+		load_child_theme_textdomain( 'framework', get_stylesheet_directory() . '/languages' );
+	}
+}
+add_action( 'after_setup_theme', 'inspiry_elegantia_child_theme_setup' );
